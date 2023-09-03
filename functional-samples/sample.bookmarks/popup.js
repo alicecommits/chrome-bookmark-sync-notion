061ded1433ebcc748d90d53b16b1f665fbfd0bbe
Alice <alice.maria.rizzo@gmail.com>
2023-09-03 12:21:03 +0100

    send msg when REST API fetch button is clicked

--- functional-samples/sample.bookmarks/popup.js
+++ functional-samples/sample.bookmarks/popup.js
@@ -4,44 +4,21 @@
 // license that can be found in the LICENSE file or at
 // https://developers.google.com/open-source/licenses/bsd
 
-
-// ALICE MODIF -------------èèèèèè------------------------------------------------
-
-// FETCH FEATURE (XHR FOR NOW) -------------------------------------------------------  
-// Using dummy JSON PlaceHolder API https://jsonplaceholder.typicode.com/posts/1
+// FETCH FEATURE -------------------------------------------------------  
 const myDummyURL = 'https://jsonplaceholder.typicode.com/posts/1';
 
-/*
-$("#fetch1").click(function() {
-  let xhr = new XMLHttpRequest();
-  xhr.open("GET", myDummyURL, true);
-  xhr.onreadystatechange = function() {
-  if (xhr.readyState == 4) {
-    // innerText does not let the attacker inject HTML elements.
-    document.getElementById("result").innerText = xhr.responseText;
-    }
-  }
-  xhr.send();
+$("#fetch").click(() => {
+  //document.getElementById("resultWhenClick").innerText = "WOW!";
+
+  //TODO: is there more straight-fwd approach than using chrome.runtime?
+  chrome.runtime.sendMessage({name: "fetchDummy"}, (response) => {
+    //Wait for Response
+    document.getElementById("resultDate").innerHTML = response.day; 
+    document.getElementById("result").innerHTML = response.desc;
+  });
 });
-*/
-
-//TODO: instead of at runtime, trigger ONLY at clicking
-// API call: version with service worker in the background 
-// Send Message To Background
-chrome.runtime.sendMessage({name: "fetchDummy"}, (response) => {
-  //Wait for Response
-
-  //console.log(response);
-  //would show "" if innerText
-  document.getElementById("resultDate").innerHTML = response.day; 
-  document.getElementById("result").innerHTML = response.desc;
-
-
-});
-
 
 // TRAVERSING + DISPLAYING JSTREE ---------------------------------------------
-
 //GET A SUBTREE - getSubTree('4922') - for Folder Test only:
 const startNodeUlSub = document.getElementById('startNodeUlSub');
 const targetId = '4922';
